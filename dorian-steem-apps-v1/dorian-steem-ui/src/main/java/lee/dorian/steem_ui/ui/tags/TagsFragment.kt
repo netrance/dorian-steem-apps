d47137f7ad52ceb276f75c942a90d29bbb9efde1
dorian <dorian@stunitas.com>
2022-08-09 00:36:53 +0900

    Applied data binding to TagsFragment

--- dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/ui/tags/TagsFragment.kt
+++ dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/ui/tags/TagsFragment.kt
@@ -5,16 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import lee.dorian.steem_ui.R
 import lee.dorian.steem_ui.databinding.FragmentTagsBinding
 
 class TagsFragment : Fragment() {
 
     private var _binding: FragmentTagsBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,17 +23,12 @@ class TagsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
+        val tagsViewModel =
             ViewModelProvider(this).get(TagsViewModel::class.java)
 
-        _binding = FragmentTagsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        _binding = DataBindingUtil.inflate(this.layoutInflater, R.layout.fragment_tags, null, false)
+        binding.viewModel = tagsViewModel
+        return binding.root
     }
 
     override fun onDestroyView() {
