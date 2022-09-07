5775679839a20d17c791fd9ea70604fc20bd0a01
dorian <dorian@stunitas.com>
2022-09-07 23:37:07 +0900

    Added libraries(Retrofit, RxJava) and Retrofit wrapper object

---
+++ dorian-steem-apps-v1/dorian-steem-data/src/main/java/lee/dorian/steem_data/retrofit/SteemClient.kt
@@ -0,0 +1,19 @@
+package lee.dorian.steem_data.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object SteemClient {
+
+    const val BASE_URL = "https://api.steemit.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .build()
+
+    // val apiService = retrofit.create(SteemService::class.java)
+
+}
