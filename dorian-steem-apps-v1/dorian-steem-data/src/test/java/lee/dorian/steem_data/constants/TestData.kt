9feb1ec91b4bc3b41b730d8b3ffe0bdad02b2a92
dorian <dorian@stunitas.com>
2022-09-12 23:05:34 +0900

    Added unit test code to test getAccounts method of SteemService interface

---
+++ dorian-steem-apps-v1/dorian-steem-data/src/test/java/lee/dorian/steem_data/constants/TestData.kt
@@ -0,0 +1,18 @@
+package lee.dorian.steem_data.constants
+
+object TestData {
+
+    val singleAccount = "dorian-mobileapp"
+    val invalidSingleAccount = "invalid10293845"
+
+    val multipleAccounts = arrayOf(
+        "dorian-mobileapp",
+        "dorian-lee"
+    )
+
+    val multipleInvalidAccounts = arrayOf(
+        "invalid10293845",
+        "invalid76787654"
+    )
+
+}
---
+++ dorian-steem-apps-v1/dorian-steem-data/src/test/java/lee/dorian/steem_data/retrofit/SteemServiceTest.kt
@@ -0,0 +1,78 @@
+package lee.dorian.steem_data.retrofit
+
+import lee.dorian.steem_data.constants.TestData
+import lee.dorian.steem_data.model.GetAccountsParams
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SteemServiceTest {
+
+    // Test case 1: Trying to get the information of a valid account.
+    @Test
+    fun getAccounts_case1() {
+        val getAccountParams = GetAccountsParams(
+            params = arrayOf(arrayOf(TestData.singleAccount)),
+            id = 1
+        )
+
+        SteemClient.apiService.getAccounts(getAccountParams).subscribe { responseEntity ->
+            assertEquals("2.0", responseEntity.jsonrpc ?: "")
+            assertNotNull("", responseEntity.result)
+            assertEquals(1, responseEntity.result?.size ?: 0)
+            assertEquals(TestData.singleAccount, responseEntity.result[0].name)
+        }
+    }
+
+    // Test case 2: Trying to get the information of an invalid account.
+    @Test
+    fun getAccounts_case2() {
+        val getAccountParams = GetAccountsParams(
+            params = arrayOf(arrayOf(TestData.invalidSingleAccount)),
+            id = 1
+        )
+
+        SteemClient.apiService.getAccounts(getAccountParams).subscribe { responseEntity ->
+            assertEquals("2.0", responseEntity.jsonrpc)
+            assertNotNull("", responseEntity.result)
+            assertEquals(0, responseEntity.result?.size)
+        }
+    }
+
+    // Test case 3: Trying to get the information of multiple valid accounts.
+    @Test
+    fun getAccounts_case3() {
+        val getAccountParams = GetAccountsParams(
+            params = arrayOf(TestData.multipleAccounts),
+            id = 1
+        )
+
+        SteemClient.apiService.getAccounts(getAccountParams).subscribe { responseEntity ->
+            assertEquals("2.0", responseEntity.jsonrpc ?: "")
+            assertNotNull("", responseEntity.result)
+            assertEquals(2, responseEntity.result?.size ?: 0)
+
+            responseEntity.result?.let {
+                for (i in 0 .. it.size - 1) {
+                    assertEquals(TestData.multipleAccounts[i], it[i].name)
+                }
+            }
+        }
+    }
+
+    // Test case 4: Trying to get the information of multiple invalid accounts.
+    @Test
+    fun getAccounts_case4() {
+        val getAccountParams = GetAccountsParams(
+            params = arrayOf(TestData.multipleInvalidAccounts),
+            id = 1
+        )
+
+        SteemClient.apiService.getAccounts(getAccountParams).subscribe { responseEntity ->
+            assertEquals("2.0", responseEntity.jsonrpc)
+            assertNotNull("", responseEntity.result)
+            assertEquals(0, responseEntity.result?.size)
+        }
+    }
+
+}
