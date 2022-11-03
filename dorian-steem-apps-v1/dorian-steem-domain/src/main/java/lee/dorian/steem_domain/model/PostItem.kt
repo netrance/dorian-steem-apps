e73e3cf028101497d5c22c5a78ada3da3ea1ac84
Dorian <netrance@naver.com>
2022-11-03 23:51:07 +0900

    PostItem data class was added, and data binding is applied between PostItem object and layout_post_item.xml layout

---
+++ dorian-steem-apps-v1/dorian-steem-domain/src/main/java/lee/dorian/steem_domain/model/PostItem.kt
@@ -0,0 +1,14 @@
+package lee.dorian.steem_domain.model
+
+data class PostItem(
+    val title: String,
+    val thumbnailURL: String,
+    val content: String,
+    val tagOrCommunity: String,
+    val time: String,
+    val rewards: Float,
+    val upvoteCount: Int,
+    val downvoteCount: Int,
+    val account: String,
+    val reputation: Int
+)
