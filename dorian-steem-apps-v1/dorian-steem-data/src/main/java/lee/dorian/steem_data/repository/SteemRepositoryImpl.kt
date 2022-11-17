a8d5f7ad7c19bed00198ba1eb0c83767aa439fb6
Dorian <netrance@naver.com>
2022-11-17 23:52:48 +0900

    Add readRankedPosts method into SteemRepository interface and. SteemRepositoryImpl class

--- dorian-steem-apps-v1/dorian-steem-data/src/main/java/lee/dorian/steem_data/repository/SteemRepositoryImpl.kt
+++ dorian-steem-apps-v1/dorian-steem-data/src/main/java/lee/dorian/steem_data/repository/SteemRepositoryImpl.kt
@@ -3,7 +3,9 @@ package lee.dorian.steem_data.repository
 import io.reactivex.Single
 import lee.dorian.steem_data.model.GetAccountsParamsDTO
 import lee.dorian.steem_data.model.GetDynamicGlobalPropertiesParamsDTO
+import lee.dorian.steem_data.model.post.GetRankedPostParamsDTO
 import lee.dorian.steem_data.retrofit.SteemClient
+import lee.dorian.steem_domain.model.PostItem
 import lee.dorian.steem_domain.model.SteemitWallet
 import lee.dorian.steem_domain.repository.SteemRepository
 
@@ -28,6 +30,30 @@ class SteemRepositoryImpl: SteemRepository {
                     )
                 )
                 else -> arrayOf()
+            }
+        }
+    }
+
+    override fun readRankedPosts(
+        sort: String,
+        tag: String,
+        observer: String,
+        limit: Int,
+        lastPostItem: PostItem?
+    ): Single<List<PostItem>> {
+        val innerParams = GetRankedPostParamsDTO.InnerParams(
+            sort,
+            tag,
+            observer,
+            limit,
+            lastPostItem?.account ?: "",
+            lastPostItem?.permlink ?: ""
+        )
+        val getRankedPostsParams = GetRankedPostParamsDTO(params = innerParams, id = 1)
+
+        return SteemClient.apiService.getRankedPosts(getRankedPostsParams).map { response ->
+            (response.result ?: listOf()).map { postItemDTO ->
+                postItemDTO.toPostItem()
             }
         }
     }
--- dorian-steem-apps-v1/dorian-steem-domain/src/main/java/lee/dorian/steem_domain/repository/SteemRepository.kt
+++ dorian-steem-apps-v1/dorian-steem-domain/src/main/java/lee/dorian/steem_domain/repository/SteemRepository.kt
@@ -1,10 +1,19 @@
 package lee.dorian.steem_domain.repository
 
 import io.reactivex.Single
+import lee.dorian.steem_domain.model.PostItem
 import lee.dorian.steem_domain.model.SteemitWallet
 
 interface SteemRepository {
 
     fun readSteemitWallet(account: String): Single<Array<SteemitWallet>>
+
+    fun readRankedPosts(
+        sort: String,
+        tag: String,
+        observer: String = "",
+        limit: Int = 20,
+        lastPostItem: PostItem? = null
+    ): Single<List<PostItem>>
 
 }
