99902b4eb8f1b2297fa5319481feede6c4d62e01
Dorian <netrance@naver.com>
2022-11-18 21:00:02 +0900

    Added a new method into SteemRepositoryImplTest class to test readRankedPosts method of SteemRepositoryImpl class

--- dorian-steem-apps-v1/dorian-steem-data/src/test/java/lee/dorian/steem_data/repository/SteemRepositoryImplTest.kt
+++ dorian-steem-apps-v1/dorian-steem-data/src/test/java/lee/dorian/steem_data/repository/SteemRepositoryImplTest.kt
@@ -1,6 +1,7 @@
 package lee.dorian.steem_data.repository
 
 import lee.dorian.steem_data.constants.TestData
+import lee.dorian.steem_data.model.post.GetRankedPostParamsDTO
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -23,6 +24,19 @@ class SteemRepositoryImplTest {
     fun readSteemitWallet_case2() {
         steemRepository.readSteemitWallet(TestData.invalidSingleAccount).subscribe { wallet ->
             assertEquals(0, wallet.size)
+        }
+    }
+
+    @Test
+    fun readRankedPosts() {
+        steemRepository.readRankedPosts("trending", "kr").subscribe { postItemList ->
+            assertEquals(postItemList.size, GetRankedPostParamsDTO.InnerParams.DEFAULT_LIMIT)
+            for (postItem in postItemList) {
+                assertTrue(postItem.account.isNotEmpty())
+                assertTrue(postItem.permlink.isNotEmpty())
+                assertTrue(postItem.title.isNotEmpty())
+                assertTrue(postItem.content.isNotEmpty())
+            }
         }
     }
 
