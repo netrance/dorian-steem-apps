18a509669c758d98ab18b71359df8a6975309435
Dorian <netrance@naver.com>
2022-11-29 22:00:21 +0900

    Apllied data binding to image view for showing thumbnail of a post item

--- dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/util/DataBindingUtil.kt
+++ dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/util/DataBindingUtil.kt
@@ -1,8 +1,14 @@
 package lee.dorian.steem_ui.util
 
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.Target
 import lee.dorian.steem_domain.model.PostItem
+import lee.dorian.steem_ui.R
 import lee.dorian.steem_ui.ui.tags.PostItemListAdapter
 
 @BindingAdapter("postItemList")
@@ -12,4 +18,12 @@ fun bind(recyclerView: RecyclerView?, postItemList: List<PostItem>?) {
             it.setList(postItemList)
         }
     }
+}
+
+@BindingAdapter("android:src")
+fun setSrc(imageView: ImageView?, url: String?) {
+    Glide.with(imageView ?: return)
+        .load(Uri.parse(url ?: return))
+        .placeholder(R.mipmap.ic_launcher)
+        .into(imageView)
 }
