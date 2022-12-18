cd0e2845cefaf3b44cc383dbb56a10afbd5b9f81
dorian <netrance@naver.com>
2022-12-18 19:19:38 +0900

    VoteListActivity: (1) Added intent key constant (2) Use different getSerializableExtra methods depending on Android version

--- dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/ui/voter/VoteListActivity.kt
+++ dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/ui/voter/VoteListActivity.kt
@@ -1,5 +1,6 @@
 package lee.dorian.steem_ui.ui.voter
 
+import android.os.Build
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import lee.dorian.steem_domain.model.ActiveVote
@@ -8,6 +9,10 @@ import lee.dorian.steem_ui.R
 import lee.dorian.steem_ui.databinding.ActivityVoteListBinding
 
 class VoteListActivity : BaseActivity<ActivityVoteListBinding, VoteListViewModel>(R.layout.activity_vote_list) {
+
+    companion object {
+        const val INTENT_BUNDLE_VOTER_LIST = "voter_list"
+    }
 
     override val viewModel: VoteListViewModel by lazy {
         ViewModelProvider(this).get(VoteListViewModel::class.java)
@@ -18,7 +23,14 @@ class VoteListActivity : BaseActivity<ActivityVoteListBinding, VoteListViewModel
         setContentView(R.layout.activity_vote_list)
 
         //val voterList = intent.getSerializableExtra("voter_list", Array<ActiveVote>::class.java)
-        val voterList = intent.getSerializableExtra("voter_list") as Array<ActiveVote>
+        val voterList = when {
+            (Build.VERSION.SDK_INT >= 33) -> {
+                intent.getSerializableExtra(INTENT_BUNDLE_VOTER_LIST, Array<ActiveVote>::class.java)
+            }
+            else -> {
+                intent.getSerializableExtra(INTENT_BUNDLE_VOTER_LIST) as Array<ActiveVote>
+            }
+        }
         binding.viewModel = viewModel
         viewModel.votes.value = voterList
         binding.listVoter.adapter = VoteListAdapter()
