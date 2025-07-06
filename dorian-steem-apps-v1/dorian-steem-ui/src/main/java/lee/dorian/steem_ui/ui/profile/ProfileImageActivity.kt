32a776fb1bc01cf5c0641ef9639cc99d1c076d82
Dorian <netrance@naver.com>
2025-07-06 22:08:41 +0900

    ProfileImageActivity Renewal: Replaced XML with Jetpack Compose

--- dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/ui/profile/ProfileImageActivity.kt
+++ dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/ui/profile/ProfileImageActivity.kt
@@ -1,41 +1,85 @@
 package lee.dorian.steem_ui.ui.profile
 
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
-import lee.dorian.steem_ui.BaseActivity
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
 import lee.dorian.steem_ui.R
-import lee.dorian.steem_ui.databinding.ActivityProfileImageBinding
-import lee.dorian.steem_ui.databinding.ActivityVoteListBinding
-import lee.dorian.steem_ui.ext.load
 
-class ProfileImageActivity : BaseActivity<ActivityProfileImageBinding, ProfileImageViewModel>(R.layout.activity_profile_image) {
+class ProfileImageActivity : AppCompatActivity() {
 
     companion object {
         const val INTENT_BUNDLE_STEEMIT_ACCOUNT = "profile_image"
     }
 
-    override val viewModel: ProfileImageViewModel by lazy {
+    val viewModel: ProfileImageViewModel by lazy {
         ViewModelProvider(this).get(ProfileImageViewModel::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.lifecycleOwner = this@ProfileImageActivity
-        viewModel.steemitAccount.apply {
-            observe(this@ProfileImageActivity, steemitAccountObserver)
-            value = intent.getStringExtra(INTENT_BUNDLE_STEEMIT_ACCOUNT) ?: ""
-        }
-
-        supportActionBar?.title = "@${viewModel.steemitAccount.value}"
+        setContentView(
+            ComposeView(this).apply {
+                setContent {
+                    val account = intent.getStringExtra(INTENT_BUNDLE_STEEMIT_ACCOUNT) ?: ""
+                    viewModel.steemitAccount.value = account
+                    supportActionBar?.title = "@${account}"
+                    ProfileImageScreen(account)
+                }
+            }
+        )
     }
 
-    private val steemitAccountObserver = Observer<String> {
-        when (it.isEmpty()) {
-            true -> binding.imageProfile.setImageResource(lee.dorian.dorian_android_ktx.R.drawable.no_image_available)
-            else -> binding.imageProfile.load("https://steemitimages.com/u/${it}/avatar/large")
-        }
+}
+
+@Composable
+fun ProfileImageScreen(account: String) {
+    val imageURL = when (account.isEmpty()) {
+        true -> ""
+        else -> "https://steemitimages.com/u/${account}/avatar/large"
     }
 
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize().background(Color.Black)
+    ) {
+        if (imageURL.isEmpty()) {
+            Image(
+                painter = painterResource(R.drawable.no_image_available),
+                contentDescription = "Image for preview"
+            )
+            return
+        }
+
+        AsyncImage(
+            model = imageURL,
+            contentDescription = "Thumbnail of the first image of this post",
+            contentScale = ContentScale.Inside,
+            error = painterResource(R.drawable.no_image_available),
+            placeholder = rememberAsyncImagePainter(R.drawable.loading),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ProfileImageScreenPreview() {
+    ProfileImageScreen("")
 }
