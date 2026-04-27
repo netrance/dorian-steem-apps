a1ea3c36298c13c2d6e89e8b4ee0cd207c7df5d5
Dorian <netrance@naver.com>
2026-04-27 19:56:27 +0900

    Jetpack Compose Migration (Step 3): Replace SplashActivity layout XML with Compose

--- dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/SplashActivity.kt
+++ dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/SplashActivity.kt
@@ -1,18 +1,31 @@
 package lee.dorian.steem_ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import lee.dorian.steem_ui.main.Main2Activity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContent {
+            SplashScreen()
+        }
 
         lifecycleScope.launch {
             delay(1300)
@@ -21,4 +34,25 @@ class SplashActivity : AppCompatActivity() {
         }
     }
 
+}
+
+@Composable
+fun SplashScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img_pircoin),
+            contentDescription = "Splash image"
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun SplashScreenPreview() {
+    SplashScreen()
 }
