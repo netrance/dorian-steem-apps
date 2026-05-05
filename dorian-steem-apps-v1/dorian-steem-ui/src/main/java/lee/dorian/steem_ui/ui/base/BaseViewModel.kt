8c096953742d60d89362fb1c83b1e468ba52aac0
Dorian <netrance@naver.com>
2026-05-05 21:01:48 +0900

    Refactor: Remove legacy dependencies (DataBinding, ViewBinding, RxJava, Navigation Fragment, etc.)

--- dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/ui/base/BaseViewModel.kt
+++ dorian-steem-apps-v1/dorian-steem-ui/src/main/java/lee/dorian/steem_ui/ui/base/BaseViewModel.kt
@@ -1,19 +1,5 @@
 package lee.dorian.steem_ui.ui.base
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
 
-open class BaseViewModel: ViewModel() {
-
-    val compositeDisposable = CompositeDisposable()
-
-    // To handle exceptions from calling APIs
-    val liveThrowable = MutableLiveData<Throwable>()
-
-    override fun onCleared() {
-        compositeDisposable.dispose()
-        super.onCleared()
-    }
-
-}
+open class BaseViewModel : ViewModel()
